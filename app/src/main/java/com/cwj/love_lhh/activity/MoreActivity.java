64dd558ce37d0cdffe105227eaaf6928d3bf8a7c
Administrator <1755140651@qq.com>
2020-03-09 10:00:13 +0800

    2020/03/09

---
+++ app/src/main/java/com/cwj/love_lhh/activity/MoreActivity.java
@@ -0,0 +1,64 @@
+package com.cwj.love_lhh.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cwj.love_lhh.R;
+import com.cwj.love_lhh.WebViewActivity;
+import com.jaeger.library.StatusBarUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MoreActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_version_number)
+    TextView tvVersionNumber;
+    @BindView(R.id.rl_baoweixingqiu)
+    RelativeLayout rlBaoweixingqiu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        StatusBarUtil.setLightMode(this);//状态栏字体暗色设置
+        tvVersionNumber.setText("V" + getLocalVersionName(this));
+    }
+
+    /**
+     * 获取本地软件版本号名称
+     */
+    public static String getLocalVersionName(Context ctx) {
+        String localVersion = "";
+        try {
+            PackageInfo packageInfo = ctx.getApplicationContext()
+                    .getPackageManager()
+                    .getPackageInfo(ctx.getPackageName(), 0);
+            localVersion = packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return localVersion;
+    }
+
+    @OnClick(R.id.rl_baoweixingqiu)
+    public void onViewClicked() {
+        Intent intent = new Intent(this, WebViewActivity.class);
+//        intent.putExtra("url", "file:///android_asset/gunqiu.html");
+        intent.putExtra("url", "http://xcdn.php.cn/js/html5/H5%203D%E6%BB%9A%E7%90%83%E6%B8%B8%E6%88%8F%E6%BA%90%E7%A0%81/index.html?sign=8602777ddf8e8801fcaac1e826ae4c48&timestamp=1583716386");
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/cwj/love_lhh/activity/WebViewActivity.java
@@ -0,0 +1,62 @@
+package com.cwj.love_lhh.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.cwj.love_lhh.R;
+import com.jaeger.library.StatusBarUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @BindView(R.id.wv)
+    WebView wv;
+    @BindView(R.id.cl_view)
+    CoordinatorLayout clView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        StatusBarUtil.setLightMode(this);//状态栏字体暗色设置
+//        StatusBarUtil.setTranslucentForImageView(this, 0, clView);//沉浸状态栏
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        /*// 设置WebView属性，能够执行Javascript脚本
+        wv.getSettings().setJavaScriptEnabled(true);
+        //语言设置防止加载乱码
+        wv.getSettings().setDefaultTextEncodingName("GBK");
+        // 即asserts文件夹下有一个color2.html
+        wv.loadUrl(url);*/
+
+        wv.getSettings().setJavaScriptEnabled(true);
+        //使网页用WebView打开
+        wv.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        wv.loadUrl(url);
+    }
+
+    @Override
+    protected void onPause() {
+        wv.reload();//关闭页面清除背景音效
+        super.onPause();
+    }
+}
