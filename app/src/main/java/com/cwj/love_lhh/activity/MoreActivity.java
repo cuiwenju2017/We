ddcaa0e6ebc7d5329019ee3f13bc896137bc996c
Administrator <1755140651@qq.com>
2020-03-12 18:20:00 +0800

    2020/03/12

--- app/src/main/java/com/cwj/love_lhh/activity/MoreActivity.java
+++ app/src/main/java/com/cwj/love_lhh/activity/MoreActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -23,6 +24,9 @@ public class MoreActivity extends AppCompatActivity {
     TextView tvVersionNumber;
     @BindView(R.id.rl_baoweixingqiu)
     RelativeLayout rlBaoweixingqiu;
+    @BindView(R.id.rl_dafeizi)
+    RelativeLayout rlDafeizi;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,11 +57,20 @@ public class MoreActivity extends AppCompatActivity {
         return localVersion;
     }
 
-    @OnClick(R.id.rl_baoweixingqiu)
-    public void onViewClicked() {
-        Intent intent = new Intent(this, WebViewActivity.class);
+    @OnClick({R.id.rl_baoweixingqiu, R.id.rl_dafeizi})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.rl_baoweixingqiu:
+                intent = new Intent(this, WebViewActivity.class);
 //        intent.putExtra("url", "file:///android_asset/gunqiu.html");
-        intent.putExtra("url", "http://xcdn.php.cn/js/html5/H5%203D%E6%BB%9A%E7%90%83%E6%B8%B8%E6%88%8F%E6%BA%90%E7%A0%81/index.html?sign=8602777ddf8e8801fcaac1e826ae4c48&timestamp=1583716386");
-        startActivity(intent);
+                intent.putExtra("url", "http://xcdn.php.cn/js/html5/H5%203D%E6%BB%9A%E7%90%83%E6%B8%B8%E6%88%8F%E6%BA%90%E7%A0%81/index.html?sign=8602777ddf8e8801fcaac1e826ae4c48&timestamp=1583716386");
+                startActivity(intent);
+                break;
+            case R.id.rl_dafeizi:
+                intent = new Intent(this, WebViewActivity.class);
+                intent.putExtra("url", "http://xcdn.php.cn/js/html5/%E6%89%8B%E6%9C%BA%E6%96%B9%E5%9D%97%E8%8B%B1%E9%9B%84%E9%97%AF%E5%85%B3%E6%B8%B8%E6%88%8F%E6%BA%90%E7%A0%81/index.html?sign=882c74c1c690f68d70fb40a9d09bc121&timestamp=1584007385");
+                startActivity(intent);
+                break;
+        }
     }
 }
