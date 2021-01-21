aa1a5f4354f3eead385b86eab2e469c1d6344d12
Administrator <1755140651@qq.com>
2021-01-21 12:16:13 +0800

    2021/01/21

--- app/src/main/java/com/cwj/we/module/ljxj/OpenCameraActivity.java
+++ app/src/main/java/com/cwj/we/module/ljxj/OpenCameraActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 
