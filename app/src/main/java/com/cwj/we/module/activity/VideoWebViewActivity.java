5b65a21ce5dd822a2aff46c96899253b9b54ee86
Administrator <1755140651@qq.com>
2021-07-05 16:50:20 +0800

    2021/07/05

--- app/src/main/java/com/cwj/we/module/activity/VideoWebViewActivity.java
+++ app/src/main/java/com/cwj/we/module/activity/VideoWebViewActivity.java
@@ -2,17 +2,12 @@ package com.cwj.we.module.activity;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.DownloadManager;
-import android.content.BroadcastReceiver;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Environment;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -30,7 +25,6 @@ import com.cwj.we.utils.ToastUtil;
 import com.gyf.immersionbar.ImmersionBar;
 import com.tencent.smtt.export.external.interfaces.WebResourceRequest;
 import com.tencent.smtt.export.external.interfaces.WebResourceResponse;
-import com.tencent.smtt.sdk.URLUtil;
 import com.tencent.smtt.sdk.WebView;
 import com.ycbjie.webviewlib.base.X5WebChromeClient;
 import com.ycbjie.webviewlib.base.X5WebViewClient;
@@ -42,7 +36,6 @@ import com.ycbjie.webviewlib.inter.VideoWebListener;
 import com.ycbjie.webviewlib.utils.X5WebUtils;
 import com.ycbjie.webviewlib.view.X5WebView;
 import com.ycbjie.webviewlib.widget.WebProgress;
-import com.ycbjie.ycupdatelib.PermissionUtils;
 
 import butterknife.BindView;
 
@@ -183,89 +176,15 @@ public class VideoWebViewActivity extends BaseActivity {
         });
 
         webView.setDownloadListener((s, s1, s2, s3, l) -> {
-            PermissionUtils.init(this);
-            boolean granted = PermissionUtils.isGranted(mPermission);
-            if (!granted) {
-                PermissionUtils permission = PermissionUtils.permission(mPermission);
-                permission.callback(new PermissionUtils.SimpleCallback() {
-                    @Override
-                    public void onGranted() {
-                        doCode(s, s1, s2, s3, l);
-                    }
-
-                    @Override
-                    public void onDenied() {
-                        PermissionUtils.openAppSettings();
-                        ToastUtil.showTextToast(VideoWebViewActivity.this, "请允许存储权限");
-                    }
-                });
-                permission.request();
-            } else {
-                doCode(s, s1, s2, s3, l);
-            }
+            doCode(s, s1, s2, s3, l);
         });
     }
 
     private void doCode(String s, String s1, String s2, String s3, long l) {
-        ToastUtil.showTextToast(this, "下载中请稍等，包大小：" + l / 1024 / 1024 + "M");
-        downloadBySystem(s, s2, s3);
-        // 使用
-        DownloadCompleteReceiver receiver = new DownloadCompleteReceiver();
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
-        registerReceiver(receiver, intentFilter);
-    }
-
-    private void downloadBySystem(String url, String contentDisposition, String mimeType) {
-        // 指定下载地址
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-        // 允许媒体扫描，根据下载的文件类型被加入相册、音乐等媒体库
-        request.allowScanningByMediaScanner();
-        // 设置通知的显示类型，下载进行时和完成后显示通知
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        // 设置通知栏的标题，如果不设置，默认使用文件名
-//        request.setTitle("This is title");
-        // 设置通知栏的描述
-//        request.setDescription("This is description");
-        // 允许在计费流量下下载
-        request.setAllowedOverMetered(false);
-        // 允许该记录在下载管理界面可见
-        request.setVisibleInDownloadsUi(false);
-        // 允许漫游时下载
-        request.setAllowedOverRoaming(true);
-        // 允许下载的网路类型
-        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI);
-        // 设置下载文件保存的路径和文件名
-        String fileName = URLUtil.guessFileName(url, contentDisposition, mimeType);
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
-//        另外可选一下方法，自定义下载路径
-//        request.setDestinationUri()
-//        request.setDestinationInExternalFilesDir()
-        final DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-        // 添加一个下载任务
-        long downloadId = downloadManager.enqueue(request);
-    }
-
-    private class DownloadCompleteReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent != null) {
-                if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(intent.getAction())) {
-                    long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
-                    DownloadManager downloadManager = (DownloadManager) context.getSystemService(DOWNLOAD_SERVICE);
-                    String type = downloadManager.getMimeTypeForDownloadedFile(downloadId);
-                    if (TextUtils.isEmpty(type)) {
-                        type = "*/*";
-                    }
-                    Uri uri = downloadManager.getUriForDownloadedFile(downloadId);
-                    if (uri != null) {
-                        Intent handlerIntent = new Intent(Intent.ACTION_VIEW);
-                        handlerIntent.setDataAndType(uri, type);
-                        context.startActivity(handlerIntent);
-                    }
-                }
-            }
-        }
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse(s));
+        startActivity(intent);
     }
 
     @Override
